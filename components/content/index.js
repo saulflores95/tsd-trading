88415c90fa316860017386e92f458ab3adccb9b3
saulpolo95 <saulpolo95@gmail.com>
2017-11-21 14:05:30 -0800

    Better Hover Effects

--- components/content/index.js
+++ components/content/index.js
@@ -58,12 +58,12 @@ export default class Content extends Component {
           </Col>
           <Col lg={6}>
             <Row>
-              <Img img='https://thumbs.dreamstime.com/b/textura-del-cemento-6536569.jpg' text='Cement' alt='cement' />
-              <Img img='https://www.harpdesignco.com/wp-content/uploads/2017/04/floor-1256804_1920-800x423.jpg' text='Wood' alt='wood'/>
-              <Img img='http://www.metromix.com.au/files/3613/0155/0120/Screen_shot_2011-03-31_at_4.41.34_PM.png' text= 'Gravel' alt='gravel'/>
-              <Img img='http://www.guoguiyan.com/data/out/4/68906191-aluminum-wallpapers.png' text='Aluminium' alt='Aluminium'/>
-              <Img img='http://estag.fimagenes.com/img/3/3/0/F/L/30FL_900.jpg' text='Block' alt='block'/>
-              <Img img='https://image.freepik.com/foto-gratis/textura-de-playa-de-arena_1232-454.jpg' text='Sand' alt='sand'/>
+              <Img img='https://thumbs.dreamstime.com/b/textura-del-cemento-6536569.jpg' text='Cement' flag='left' alt='cement' />
+              <Img img='https://www.harpdesignco.com/wp-content/uploads/2017/04/floor-1256804_1920-800x423.jpg' text='Wood'  flag='right' alt='wood'/>
+              <Img img='http://www.metromix.com.au/files/3613/0155/0120/Screen_shot_2011-03-31_at_4.41.34_PM.png' text= 'Gravel' flag='left' alt='gravel'/>
+              <Img img='http://www.guoguiyan.com/data/out/4/68906191-aluminum-wallpapers.png' text='Aluminium' flag='right' alt='Aluminium'/>
+              <Img img='http://estag.fimagenes.com/img/3/3/0/F/L/30FL_900.jpg' text='Block' flag='left' alt='block'/>
+              <Img img='https://image.freepik.com/foto-gratis/textura-de-playa-de-arena_1232-454.jpg' text='Sand' flag='right' alt='sand'/>
             </Row>
           </Col>
         </Row>
--- components/img/index.js
+++ components/img/index.js
@@ -1,59 +1,120 @@
-  import { Col } from 'react-grid-system'
+import {Component } from 'react'
+import { Col } from 'react-grid-system'
 
-const Img = (props) => (
-  <Col lg={6} md={6} sm={12}>
-  {console.log(props)}
-    <div className='container'>
-      <img src={props.img} alt='Avatar' className='image' />
-      <div className='overlay'>
-        <div className='text'>{props.text}</div>
-      </div>
-    </div>
-    <style jsx>{`
-      .container {
-        position: relative;
-        height:300px;
-        margin-top:10px;
-      }
+export default class Img  extends Component {
+  renderChecker(props) {
+    if(props.flag === 'right') {
+      return (
+        <Col lg={6} md={6} sm={12}>
+          <div className='container'>
+            <img src={props.img} alt='Avatar' className='image' />
+            <div className='overlay'>
+              <div className='text'>{props.text}</div>
+            </div>
+          </div>
+          <style jsx>{`
+            .container {
+              position: relative;
+              width: 100%;
+              height: 300px;
+            }
 
-      .image {
-        display: block;
-        width: 100%;
-        height: 100%;
-      }
+            .image {
+              display: block;
+              width: 100%;
+              height: 100%;
+            }
 
-      .overlay {
-        position: absolute;
-        bottom: 100%;
-        left: 0;
-        right: 0;
-        background-color: black;
-        overflow: hidden;
-        width: 100%;
-        height:0;
-        transition: .5s ease;
-        opacity: 0.5;
+            .overlay {
+              position: absolute;
+              bottom: 0;
+              left: 0;
+              right: 0;
+              background-color: black;
+              overflow: hidden;
+              width: 0;
+              height: 100%;
+              transition: .5s ease;
+              opacity: 0.5;
+            }
 
-      }
+            .container:hover .overlay {
+              width: 100%;
+            }
 
-      .container:hover .overlay {
-        bottom: 0;
-        height: 100%;
-      }
+            .text {
+              white-space: nowrap;
+              color: white;
+              font-size: 20px;
+              position: absolute;
+              overflow: hidden;
+              top: 50%;
+              left: 50%;
+              transform: translate(-50%, -50%);
+              -ms-transform: translate(-50%, -50%);
+            }
+          `}</style>
+        </Col>
+      )
+    } else {
+      return (
+        <Col lg={6} md={6} sm={12}>
+          <div className='container'>
+            <img src={props.img} alt='Avatar' className='image' />
+            <div className='overlay'>
+              <div className='text'>{props.text}</div>
+            </div>
+          </div>
+          <style jsx>{`
+            .container {
+              position: relative;
+              width: 100%;
+              height: 300px;
+            }
+            .image {
+              display: block;
+              width: 100%;
+              height: 100%;
+            }
 
-      .text {
-        white-space: nowrap;
-        color: white;
-        font-size: 20px;
-        position: absolute;
-        overflow: hidden;
-        top: 50%;
-        left: 50%;
-        transform: translate(-50%, -50%);
-        -ms-transform: translate(-50%, -50%);
-      }
-    `}</style>
-  </Col>
-)
+            .overlay {
+              position: absolute;
+              bottom: 0;
+              left: 100%;
+              right: 0;
+              background-color: black;
+              overflow: hidden;
+              width: 0;
+              height: 100%;
+              transition: .5s ease;
+              opacity: 0.5;
 
-export default Img
+            }
+
+            .container:hover .overlay {
+              width: 100%;
+              left: 0;
+            }
+
+            .text {
+              white-space: nowrap;
+              color: white;
+              font-size: 20px;
+              position: absolute;
+              overflow: hidden;
+              top: 50%;
+              left: 50%;
+              transform: translate(-50%, -50%);
+              -ms-transform: translate(-50%, -50%);
+            }
+          `}</style>
+        </Col>
+      )
+    }
+  }
+  render() {
+    return (
+      this.renderChecker(this.props)
+    )
+  }
+}
